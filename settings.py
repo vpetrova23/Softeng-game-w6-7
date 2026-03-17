fcb2b654e1f458ffc85a4c074961cb8911d9a46d
Viola <viola.petrova@student.uva.nl>
2026-03-17 16:28:36 +0100

    Hierbij de settings.py :)

---
+++ settings.py
@@ -0,0 +1,43 @@
+#De basis instellingen van het spel
+
+#Scherm instellingen
+# ---------------------------
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+FPS = 60
+
+# Wereld instellingen
+# ---------------------------
+WORLD_WIDTH = 5200
+WORLD_HEIGHT = 5200
+BACKGROUND_COLOR = (255, 255, 255)
+GRID_COLOR = (220, 220, 220)
+
+# Speler instellingen
+# ---------------------------
+PLAYER_START_RADIUS = 40
+PLAYER_START_SPEED = 10
+PLAYER_COLOR = (255, 182, 193)
+
+# Enemy instellingen 
+# ---------------------------
+ENEMY_START_RADIUS = 30
+ENEMY_START_SPEED = 10
+ENEMY_COUNT = 10
+
+
+# Voedsel instellingen
+# ---------------------------
+FOOD_RADIUS = 10
+FOOD_COUNT = 300
+
+# Spelregels
+# ---------------------------
+GROWTH_RATE = 0.5
+EAT_RATIO = 1.1
+SPEED_DECREASE_RATE = 0.001
+
+
+
+
+
